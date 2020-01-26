922a610032015188ffaeed8ef9b905b7131054ff
sankot <sankot24@gmail.com>
2020-01-26 14:57:24 +0300

    Solved numberRevert, accountInThreeYears, travelMinutes, thirdDigit, angleRadian

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -51,6 +51,10 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 fun main() {
     val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
     println("Root product: $x1x2")
+
+
+    println("Total sum is:  ${accountInThreeYears(100, 10)}");
+    println("Revert of 345 is:  ${numberRevert(345)}");
 }
 
 /**
@@ -76,7 +80,9 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    return PI / (60 * 60 * 180) * (deg * 3600 + sec + min * 60);
+}
 
 /**
  * Тривиальная
@@ -92,7 +98,9 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+     return number / 100 % 10;
+}
 
 /**
  * Простая
@@ -101,7 +109,11 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int):Int  {
+    val timeStart: Int = hoursDepart * 60 + minutesDepart;
+    val timeEnd: Int = hoursArrive * 60 + minutesArrive;
+    return timeEnd - timeStart;
+}
 
 /**
  * Простая
@@ -110,7 +122,14 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    var sum: Double = 1.0 * initial;
+    var range = 0..2;
+    for (i in range) {
+        sum += sum * percent / 100;
+    }
+    return sum;
+}
 
 /**
  * Простая
@@ -118,4 +137,12 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    var dst: Int = 0; //
+    var tmp: Int = number;
+    while (tmp > 0) {
+        dst = dst * 10 + tmp % 10;
+        tmp /= 10
+    }
+    return dst // Результат
+}
